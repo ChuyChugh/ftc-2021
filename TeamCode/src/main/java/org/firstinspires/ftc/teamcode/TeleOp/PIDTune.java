38f89c5f544f9a3ce98159e4dddb8b02f862babe
Srihith Reddy <pallamreddysrihith@gmail.com>
2021-02-23 23:49:40 -0500

    Update Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PIDTune.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PIDTune.java
@@ -1,105 +1,149 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
-import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
-import org.firstinspires.ftc.teamcode.Dependencies.TeleBot;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Tune PID Constants")
+import java.util.Date;
+
+@TeleOp(name="tune pid shooter")
 public class PIDTune extends LinearOpMode {
-    Motor frontLeft, frontRight, backLeft, backRight;
-    Motor intake, shooter, grabberLift;
-    RevIMU imu;
-    MecanumDrive m_drive;
-    VoltageSensor voltageSensor;
-    TeleBot robot;
-    CRServo flicker;
-    SimpleServo grabber;
-    PIDController pid = new PIDController(0, 0, 0);
+
+    private Motor fL, fR, bL, bR, intake;
+    private Motor shooterF, shooterB;
+    private SimpleServo hopper, flicker,grabber, gearboxL, gearboxR;
+    private ElapsedTime time;
+    private RevIMU imu;
+    private MecanumDrive drive;
+    private VoltageSensor voltageSensor;
+    private GamepadEx gamepad;
+    private ButtonReader flickerController, incSpeedController, decSpeedController;
+    private ToggleButtonReader gearboxController, hopperController, grabberController;
 
     @Override
+
     public void runOpMode() throws InterruptedException {
 
-        frontLeft = new Motor(hardwareMap, "fL");
-        frontRight = new Motor(hardwareMap, "fR");
-        backLeft = new Motor(hardwareMap, "bL");
-        backRight = new Motor(hardwareMap, "bR");
+        fL = new Motor(hardwareMap, "fL");
+        fR = new Motor(hardwareMap, "fR");
+        bL = new Motor(hardwareMap, "bL");
+        bR = new Motor(hardwareMap, "bR");
+        drive = new MecanumDrive(fL, fR, bL, bR);
+
+
+        shooterF = new MotorEx(hardwareMap, "shooterF");
+        shooterB = new MotorEx(hardwareMap, "shooterB");
         intake = new Motor(hardwareMap, "intake");
-        shooter = new Motor(hardwareMap, "shooter");
-        flicker = new CRServo(hardwareMap, "flicker");
-        grabberLift = new Motor(hardwareMap, "grabberLift");
+
+        flicker = new SimpleServo(hardwareMap, "flicker", -90,90);
+        hopper = new SimpleServo(hardwareMap, "hopper", -90, 180);
         grabber = new SimpleServo(hardwareMap, "grabber", -90, 180);
+        gearboxL = new SimpleServo(hardwareMap, "gearboxL", -90, 180);
+        gearboxR = new SimpleServo(hardwareMap, "gearboxR",-90,180);
 
-        imu = new RevIMU(hardwareMap, "imu");
+        time = new ElapsedTime();
+        flicker.setInverted(true);
+        flicker.setPosition(0.5);
 
-        robot = new TeleBot(frontLeft, frontRight, backLeft, backRight, intake, shooter, grabberLift, grabber, imu, flicker);
-        robot.initialize();
+        shooterF.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        shooterB.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        fL.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        fR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        bL.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        bR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        intake.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        hopper.setPosition(1-(40.0/270));
+        gearboxR.setPosition(0.4);
+        gearboxL.setPosition(0.4);
+        grabber.setPosition(0);
 
         waitForStart();
+
+        shooterF.setRunMode(Motor.RunMode.VelocityControl);
+        shooterB.setRunMode(Motor.RunMode.VelocityControl);
+        shooterB.setVeloCoefficients(0, 0, 0);
+        shooterF.setVeloCoefficients(0, 0, 0);
+
+        shooterF.set(0.5);
+        shooterB.set(0.5);
+
         double p = 0;
         double i = 0;
         double d = 0;
         double increment = 0.1;
-
-        while (opModeIsActive() && !isStopRequested()) {
-            pid.setSetPoint(10);
-            shooter.resetEncoder();
-            shooter.set(pid.calculate(shooter.getCurrentPosition()));
-
-            telemetry.addData("Ticks Per Loop Run", shooter.getCurrentPosition());
-            telemetry.addData("Increment Level", increment);
-            telemetry.addData("P Constant", p);
-            telemetry.addData("I Constant", i);
-            telemetry.addData("D Constant", d);
-
-            telemetry.update();
+        while(opModeIsActive() && !isStopRequested()){
 
             if (gamepad1.dpad_up){
                 p += increment;
-                pid.setP(p);
+                shooterF.setVeloCoefficients(p, i , d);
+                shooterB.setVeloCoefficients(p, i , d);
                 sleep(500);
             }
             if (gamepad1.dpad_down){
                 p -= increment;
-                pid.setP(p);
+                shooterF.setVeloCoefficients(p, i , d);
+                shooterB.setVeloCoefficients(p, i , d);
                 sleep(500);
             }
             if (gamepad1.dpad_left){
                 i -= increment;
-                pid.setI(i);
+                shooterF.setVeloCoefficients(p, i , d);
+                shooterB.setVeloCoefficients(p, i , d);
                 sleep(500);
             }
             if (gamepad1.dpad_right){
                 i += increment;
-                pid.setI(i);
+                shooterF.setVeloCoefficients(p, i , d);
+                shooterB.setVeloCoefficients(p, i , d);
                 sleep(500);
             }
             if (gamepad1.left_bumper){
                 d -= increment;
-                pid.setD(d);
+                shooterF.setVeloCoefficients(p, i , d);
+                shooterB.setVeloCoefficients(p, i , d);
                 sleep(500);
             }
             if (gamepad1.right_bumper){
                 d += increment;
-                pid.setD(d);
+                shooterF.setVeloCoefficients(p, i , d);
+                shooterB.setVeloCoefficients(p, i , d);
                 sleep(500);
             }
             if (gamepad1.a){
                 increment = 0.01;
             }
-            if (gamepad1.b){
+            if (gamepad1.b) {
                 increment = 0.1;
             }
-        }
+
+            shooterB.resetEncoder();
+            shooterF.resetEncoder();
+            long startTime = System.currentTimeMillis();
+            long elapsedTime = 0L;
+
+            while (elapsedTime < 1000 && opModeIsActive() && !isStopRequested()){
+                elapsedTime = (new Date()).getTime() - startTime;
+            }
+            telemetry.addData("Ticks Per Second Back", shooterB.getCurrentPosition());
+            telemetry.addData("Ticks Per Second Front", shooterF.getCurrentPosition());
+            telemetry.addData("Increment Level", increment);
+            telemetry.addData("P Constant", p);
+            telemetry.addData("I Constant", i);
+            telemetry.addData("D Constant", d);
+            telemetry.update();
+       }
     }
 }
+
+
