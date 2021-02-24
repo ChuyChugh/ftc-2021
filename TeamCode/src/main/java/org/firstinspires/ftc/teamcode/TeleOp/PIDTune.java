f0ed8575ad9906b70894b001fc04f76ed31d8e25
Srihith Reddy <pallamreddysrihith@gmail.com>
2021-02-24 12:33:06 -0500

    Update PIDTune.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PIDTune.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PIDTune.java
@@ -1,71 +1,28 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
-import com.arcrobotics.ftclib.hardware.RevIMU;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.Date;
 
 @TeleOp(name="tune pid shooter")
 public class PIDTune extends LinearOpMode {
 
-    private Motor fL, fR, bL, bR, intake;
     private Motor shooterF, shooterB;
-    private SimpleServo hopper, flicker,grabber, gearboxL, gearboxR;
-    private ElapsedTime time;
-    private RevIMU imu;
-    private MecanumDrive drive;
-    private VoltageSensor voltageSensor;
     private GamepadEx gamepad;
     private ButtonReader incrementUp, incrementDown, pUp, pDown, iUp, iDown, dUp, dDown;
 
     @Override
 
     public void runOpMode() throws InterruptedException {
-
-        fL = new Motor(hardwareMap, "fL");
-        fR = new Motor(hardwareMap, "fR");
-        bL = new Motor(hardwareMap, "bL");
-        bR = new Motor(hardwareMap, "bR");
-        drive = new MecanumDrive(fL, fR, bL, bR);
         gamepad = new GamepadEx(gamepad1);
 
-        shooterF = new MotorEx(hardwareMap, "shooterF");
-        shooterB = new MotorEx(hardwareMap, "shooterB");
-        intake = new Motor(hardwareMap, "intake");
-
-        flicker = new SimpleServo(hardwareMap, "flicker", -90,90);
-        hopper = new SimpleServo(hardwareMap, "hopper", -90, 180);
-        grabber = new SimpleServo(hardwareMap, "grabber", -90, 180);
-        gearboxL = new SimpleServo(hardwareMap, "gearboxL", -90, 180);
-        gearboxR = new SimpleServo(hardwareMap, "gearboxR",-90,180);
-
-        time = new ElapsedTime();
-        flicker.setInverted(true);
-        flicker.setPosition(0.5);
-
-        shooterF.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        shooterB.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        fL.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        fR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        bL.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        bR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        intake.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-
-        hopper.setPosition(1-(40.0/270));
-        gearboxR.setPosition(0.4);
-        gearboxL.setPosition(0.4);
-        grabber.setPosition(0);
+        shooterF = new Motor(hardwareMap, "shooterF");
+        shooterB = new Motor(hardwareMap, "shooterB");
 
         pUp = new ButtonReader(gamepad, GamepadKeys.Button.DPAD_UP);
         pDown = new ButtonReader(gamepad, GamepadKeys.Button.DPAD_DOWN);
@@ -144,12 +101,12 @@ public class PIDTune extends LinearOpMode {
             long startTime = System.currentTimeMillis();
             long elapsedTime = 0L;
 
-            while (just_pressed && elapsedTime < 1000 && opModeIsActive() && !isStopRequested()){
+            while (just_pressed && elapsedTime < 1000 && opModeIsActive() && !isStopRequested()) {
                 elapsedTime = (new Date()).getTime() - startTime;
             }
 
             just_pressed = false;
-
+            
             telemetry.addData("Ticks Per Second Back", shooterB.getCurrentPosition());
             telemetry.addData("Ticks Per Second Front", shooterF.getCurrentPosition());
             telemetry.addData("Increment Level", increment);
@@ -157,6 +114,7 @@ public class PIDTune extends LinearOpMode {
             telemetry.addData("I Constant", i);
             telemetry.addData("D Constant", d);
             telemetry.update();
+            sleep(3000);
        }
     }
 }
